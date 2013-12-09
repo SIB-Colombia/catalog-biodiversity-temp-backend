aeb10763e8b24afc0910343e020c5973878e8284
Laura Valentina Grajales Olarte <valegrajales@gmail.com>
2013-12-09 08:22:52 -0500

    Deleting testing php file for security reasons

--- prueba.php
+++
@@ -1,5 +0,0 @@
-<?php
-	$connectionStringConstruct = 'pgsql:host='.getenv('CATALOG_BACKEND_DBHOST').';port=5432;dbname='.getenv('CATALOG_BACKEND_DBNAME');
-	echo $connectionStringConstruct;
-	phpinfo();
-?>
